36fc1f5670a09319ae69ad3747c39929d4644695
Aayush shrestha <aayushrestha5@gmail.com>
2023-05-16 20:47:06 +0545

    amination changed and user can go to next level

--- game.js
+++ game.js
@@ -1,19 +1,69 @@
 var buttonColors=["red", "blue", "green", "yellow"];
 
 var gamePattern=[];
-
-
-
+var userClickedPattern=[];
+var started=false;
+var level = 0;
 
 function nextSequence(){
+
+    userClickedPattern=[];
+    
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    console.log(gamePattern.length);
+
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);   
-    gamePattern.push(randomChosenColor);
-}
+};
 
 function playSound(name){
     var audio = new Audio("sounds/"+ name +".mp3");
     audio.play();
-}
+};
+
+$(".btn").click(function(){
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    console.log(userClickedPattern.length);
+    checkAnswer(userClickedPattern.length-1);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed");
+    },100);
+};
+
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started=true;
+    }
+});
+
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]  ){
+        console.log("success");
+
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+
+    }else
+    console.log("wrong");
+};
